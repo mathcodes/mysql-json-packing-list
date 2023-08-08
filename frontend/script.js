b5865776d339880d05bea9da61d04129bdc067e0
Jon Christie <jonpchristie@gmail.com>
2023-08-08 02:37:07 -0400

    initial

--- frontend/script.js
+++ frontend/script.js
@@ -34,34 +34,37 @@ function createItemPrompt() {
   });
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  loadPackingList();
+});
+
 function loadPackingList() {
   // Call the API endpoint to get the list of items
   fetch('/list', {
-      method: 'GET'
+    method: 'GET'
   })
   .then(response => response.json())
   .then(data => {
-      // Populate the itemTypes div with the data
-      const itemTypesRow = document.getElementById('itemTypes');
-      itemTypesRow.innerHTML = ""; // Clear existing items
-      for (let category in data) {
-          data[category].forEach(item => {
-              // Create a new row for each item and add to the itemTypes row
-              const itemRow = document.createElement('tr');
-              itemRow.innerHTML = `
-                  <td>${item.item_type}</td>
-                  <td>${item.item_name}</td>
-                  <td>${item.quantity}</td>
-                  <td>${item.colors}</td>
-                  <td>${item.importance}</td>
-              `;
-              itemTypesRow.appendChild(itemRow);
-          });
-      }
+    // Populate the itemTypes div with the data
+    const itemTypesRow = document.getElementById('itemTypes');
+    itemTypesRow.innerHTML = ""; // Clear existing items
+    for (let category in data) {
+      data[category].forEach(item => {
+        // Create a new row for each item and add to the itemTypes row
+        const itemRow = document.createElement('tr');
+        itemRow.innerHTML = `
+          <td>${item.item_type}</td>
+          <td>${item.item_name}</td>
+          <td>${item.quantity}</td>
+          <td>${item.colors}</td>
+          <td>${item.importance}</td>
+        `;
+        itemTypesRow.appendChild(itemRow);
+      });
+    }
   })
   .catch(error => {
-      // Handle error
-      console.error("Error loading packing list:", error);
+    // Handle error
+    console.error("Error loading packing list:", error);
   });
 }
-
